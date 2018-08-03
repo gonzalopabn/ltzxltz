8c9684addf36e8f3c95c9f9a3f47e7ae5b39bc46
gonzalopabn <gonzalo.mtzavila@gmail.com>
2018-08-03 10:49:29 -0700

    Cambios en la noche incluyendo el tiempo, conteo, y ciclos if

--- vtn.py
+++ vtn.py
@@ -16,29 +16,29 @@ print("connect successful!!")
 root = Tk()
 #root.attributes('-fullscreen', True)
 root.title("Ventana Prueba")
-root.geometry('800x600')
+root.geometry('720x400')
 ### Etiqueta de titulo ###
-label_1= Label(root, text="Cuentalitros pa' las fiestas PABN",bg="blue", fg="white", anchor=CENTER, height= "3", font="Roboto 20")
+label_1= Label(root, text="Cuentalitros pa' las fiestas PABN",bg="blue", fg="white", anchor=CENTER, height= "2", font="Roboto 20")
 label_1.pack(fill=X)
 abst = 0
 ### Text Box ###
 #entry_1 = Text(root, height=4, width=30)
 #entry_1.place(x=100,y=160)
 label_5 = Label(root, text="Conteo Actual", bg="red", fg="black", anchor=CENTER, height="2", width="18", font="Roboto 20")
-label_5.place(x=100,y=160)
+label_5.place(x=100,y=100)
 label_6 = Label(root, text="Conteo Actual", bg="red", fg="black", anchor=CENTER, height="2", width="18", font="Roboto 20")
-label_6.place(x=100,y=290)
+label_6.place(x=100,y=230)
 label_7 = Label(root, text="Conteo Actual", bg="red", fg="black", anchor=CENTER, height="2", width="18", font="Roboto 20")
-label_7.place(x=100,y=420)
+label_7.place(x=100,y=360)
 
 
 ### Etiquetas Textbox ###
 label_2 = Label(root, text="Conteo Actual", bg="white", anchor=CENTER, height="2", width="20", font="Roboto 20")
-label_2.place(x=400,y=160)
+label_2.place(x=400,y=100)
 label_3 = Label(root, text="Conteo Diario", bg="white", anchor=CENTER, height="2", width="20", font="Roboto 20")
-label_3.place(x=400,y=290)
+label_3.place(x=400,y=230)
 label_4 = Label(root, text="Conteo Semanal", bg="white", anchor=CENTER, height="2", width="20", font="Roboto 20")
-label_4.place(x=400,y=420)
+label_4.place(x=400,y=360)
 
 a = 0
 b = 0
